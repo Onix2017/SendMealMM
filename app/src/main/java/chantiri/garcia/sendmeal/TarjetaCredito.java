1510eabc3bd11b679f204317d2cd7938604b7054
Martin <martin__g@hotmail.com>
2019-09-09 09:53:22 -0300

    Version Martin 09/09

--- app/src/main/java/chantiri/garcia/sendmeal/TarjetaCredito.java
+++ app/src/main/java/chantiri/garcia/sendmeal/TarjetaCredito.java
@@ -3,21 +3,5 @@ package chantiri.garcia.sendmeal;
 import androidx.annotation.Nullable;
 
 public class TarjetaCredito {
-    public TarjetaCredito() {
-    }
 
-    @Override
-    public String toString() {
-        return "TarjetaCredito{}";
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(@Nullable Object obj) {
-        return super.equals(obj);
-    }
 }
--- app/src/main/java/chantiri/garcia/sendmeal/Usuario.java
+++ app/src/main/java/chantiri/garcia/sendmeal/Usuario.java
@@ -3,22 +3,5 @@ package chantiri.garcia.sendmeal;
 import androidx.annotation.Nullable;
 
 public class Usuario {
-    public Usuario() {
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(@Nullable Object obj) {
-        return super.equals(obj);
-    }
-
-    @Override
-    public String toString() {
-        return "Usuario{}";
-    }
 
 }
