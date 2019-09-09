2ae1fd85e7aeaf0621be83cd1086c6d74c414350
Martin <martin__g@hotmail.com>
2019-09-09 09:45:57 -0300

    Version Martin 09/09

---
+++ app/src/main/java/chantiri/garcia/sendmeal/Usuario.java
@@ -0,0 +1,24 @@
+package chantiri.garcia.sendmeal;
+
+import androidx.annotation.Nullable;
+
+public class Usuario {
+    public Usuario() {
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{}";
+    }
+
+}
