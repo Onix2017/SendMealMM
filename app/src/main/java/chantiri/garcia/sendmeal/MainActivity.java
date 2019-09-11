4f0ff6d3a2cd600f0bd46fb43b04eba9ad3a94de
Mauricio <mauriciochantiri@yahoo.com.ar>
2019-09-10 21:15:39 -0300

    Switch Vendedor

--- app/src/main/java/chantiri/garcia/sendmeal/MainActivity.java
+++ app/src/main/java/chantiri/garcia/sendmeal/MainActivity.java
@@ -6,11 +6,13 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
     private EditText Nombre, Contraseña, ReContraseña,Email;
+    private Switch SwVend;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -21,6 +23,21 @@ public class MainActivity extends AppCompatActivity {
         Contraseña = (EditText) findViewById(R.id.regClave);
         ReContraseña = (EditText) findViewById(R.id.editText2);
         Email = (EditText) findViewById(R.id.regMail);
+
+
+    }
+
+    public void Vendedor(View v){
+        SwVend = (Switch) findViewById(R.id.SwVendedor);
+
+        if(SwVend.isChecked())
+        {
+            Toast.makeText(this,"Encendido",Toast.LENGTH_LONG).show();
+        }else
+        {
+            Toast.makeText(this,"Apagado",Toast.LENGTH_LONG).show();
+        }
+
     }
 
     public void enviar(View v){
@@ -31,10 +48,10 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this,"Campo Contraseña vacio",Toast.LENGTH_LONG).show();
             }else {
                 if(ReContraseña.getText().toString().isEmpty()){
-                    Toast.makeText(this,"Campo Nombre vacio",Toast.LENGTH_LONG).show();
+                    Toast.makeText(this,"Campo validar contraseña vacio",Toast.LENGTH_LONG).show();
                 }else {
                     if(Email.getText().toString().isEmpty()) {
-                        Toast.makeText(this,"Campo Nombre vacio",Toast.LENGTH_LONG).show();
+                        Toast.makeText(this,"Campo E-Mail vacio",Toast.LENGTH_LONG).show();
                     }else {
                         if(ReContraseña.getText().toString()!=Contraseña.getText().toString()) {
                             Toast.makeText(this,"Verifique validacion contraseña",Toast.LENGTH_LONG).show();
