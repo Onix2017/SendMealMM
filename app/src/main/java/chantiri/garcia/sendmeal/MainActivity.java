bdb507663204381bdc5f907d5dd2480297ad0dec
Martin <martin__g@hotmail.com>
2019-09-11 11:33:51 -0300

    Martin 11/09

--- app/src/main/java/chantiri/garcia/sendmeal/MainActivity.java
+++ app/src/main/java/chantiri/garcia/sendmeal/MainActivity.java
@@ -5,14 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
-    private EditText Nombre, Contraseña, ReContraseña,Email;
+    private EditText Nombre, Contraseña, ReContraseña,Email, NroTarjeta,CodTarjeta,MMAATarjeta;
     private Switch SwVend;
+    private LinearLayout LayCuenta;
+    private CheckBox AceptaTerminos;
+    private Button Registrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -21,24 +27,39 @@ public class MainActivity extends AppCompatActivity {
 
         Nombre = (EditText) findViewById(R.id.regNombre);
         Contraseña = (EditText) findViewById(R.id.regClave);
-        ReContraseña = (EditText) findViewById(R.id.editText2);
+        ReContraseña = (EditText) findViewById(R.id.regClave2);
         Email = (EditText) findViewById(R.id.regMail);
-
-
+        LayCuenta = (LinearLayout) findViewById(R.id.layoutCuenta);
+        NroTarjeta = (EditText) findViewById(R.id.regNro);
+        CodTarjeta = (EditText) findViewById(R.id.regDigito);
+        MMAATarjeta = (EditText) findViewById(R.id.regVto);
+        AceptaTerminos = (CheckBox) findViewById(R.id.aceptaCondiciones);
+        Registrar = (Button) findViewById(R.id.btnRegistrar);
     }
 
     public void Vendedor(View v){
-        SwVend = (Switch) findViewById(R.id.SwVendedor);
+        SwVend = (Switch) findViewById(R.id.optEsVendedor);
 
         if(SwVend.isChecked())
         {
-            Toast.makeText(this,"Encendido",Toast.LENGTH_LONG).show();
+            LayCuenta.setVisibility(View.VISIBLE);
+          //  Toast.makeText(this,"Encendido",Toast.LENGTH_LONG).show();
         }else
         {
-            Toast.makeText(this,"Apagado",Toast.LENGTH_LONG).show();
+            LayCuenta.setVisibility(View.GONE);
+            //Toast.makeText(this,"Apagado",Toast.LENGTH_LONG).show();
         }
 
     }
+
+    public void AceptaTerminos(View v){
+        if(AceptaTerminos.isChecked()) {
+            Registrar.setEnabled(true);
+        }else {
+            Registrar.setEnabled(false);
+        }
+    }
+
 
     public void enviar(View v){
         if(Nombre.getText().toString().isEmpty()) {
