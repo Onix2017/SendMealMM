4843de8e3d86db898b988110763b841dfad4fa5a
Mauricio <mauriciochantiri@yahoo.com.ar>
2019-09-22 16:53:06 -0300

    Clase

--- app/src/main/java/chantiri/garcia/sendmeal/TarjetaCredito.java
+++ app/src/main/java/chantiri/garcia/sendmeal/TarjetaCredito.java
@@ -1,7 +1,15 @@
 package chantiri.garcia.sendmeal;
 
+import android.os.Bundle;
+
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class TarjetaCredito {
+public class TarjetaCredito extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
--- app/src/main/java/chantiri/garcia/sendmeal/home.java
+++ app/src/main/java/chantiri/garcia/sendmeal/home.java
@@ -3,9 +3,12 @@ package chantiri.garcia.sendmeal;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class home extends AppCompatActivity {
@@ -22,6 +25,18 @@ public class home extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.items_act2,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+
+        switch (item.getItemId()){
+            case R.id.mnuOpt1:
+               // Toast.makeText(this, "Opcion Registrarse", Toast.LENGTH_LONG).show();
+                Intent i1 = new Intent(this, TarjetaCredito.class);
+                startActivity(i1);
+                return true;
+        }
         return true;
     }
 }
